bb62eb14f4e3f8c85038db782359bb94b54511c6
Jahaziel <jahaziel.vaz@gmail.com>
2018-05-06 02:03:13 -0600

    Validation part 1

--- validator/register.js
+++ validator/register.js
@@ -1,15 +1,17 @@
-const validator = require('validator');
+const Validator = require('validator');
+import isEmpty from './is-empty'
 
 
 
 module.exports = function validateRegisterInput(data){
   let errors = {};
 
-  if(!validator.isLength(data.name, {min: 2, max: 30})){
+  if(!Validator.isLength(data.name, {min: 2, max: 30})){
     errors.name = "Name must be between 2 and 30 characters"
   }
 
   return{
-    errors: 
+    errors,
+    isValid: isEmpty(errors);
   }
 }
