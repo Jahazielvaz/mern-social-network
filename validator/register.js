3a8120e61c12a44dc631242306b0ea7978cc9ecc
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-05-10 18:22:22 -0600

    Extra validation

--- validator/register.js
+++ validator/register.js
@@ -7,11 +7,11 @@ module.exports = function validateRegisterInput(data){
   let errors = {};
 
   if(!Validator.isLength(data.name, {min: 2, max: 30})){
-    errors.name = "Name must be between 2 and 30 characters"
+    errors.name = "Name must be between 2 and 30 characters";
   }
 
   return{
     errors,
     isValid: isEmpty(errors)
-  }
-}
+  };
+};
